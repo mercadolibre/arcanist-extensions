bb5570b49dcfc9c7c118a6ef76fa16f352fce7f5
Juan Pablo Civile <jpcivile@monits.com>
2015-07-13 18:53:54 -0300

    Set name for ESLint messages
    Summary: Transformed the end of the description into a rule name for the message.
    
    Test Plan:  - Run on umewin web, check the arc lint output
    
    Reviewers: jmsotuyo
    
    Reviewed By: jmsotuyo
    
    Maniphest Tasks: T544
    
    Differential Revision: http://ph.monits.com/D11824

--- src/lint/linter/ESLintLinter.php
+++ src/lint/linter/ESLintLinter.php
@@ -47,6 +47,7 @@ final class ArcanistESLintLinter extends ConfigPathLinter {
                 . 'linter output. Aborting.');
         }
 
+        // This looks suspiciosly like the checkstyle output, but it's stlight different...
         $files = $report_dom->getElementsByTagName('file');
         foreach ($files as $file) {
             foreach ($file->childNodes as $child) {
@@ -61,15 +62,28 @@ final class ArcanistESLintLinter extends ConfigPathLinter {
                     $prefix = 'W';
                 }
 
-                $code = 'ESLINT.'.$prefix.'.'.$child->getAttribute('message');
+                // All descriptions end with (rule-name), make into Rule name
+                $description = $child->getAttribute('message');
+                if (preg_match('/\(([^)]+)\)$/', $description, $messageMatches)) {
+                    $rule = $messageMatches[1];
+                    $words = explode('-', $rule);
+                    $name = ucfirst(strtolower(implode(' ', $words)));
+                } else {
+                    $rule = 'Unnamed';
+                    $name = 'ESLint';
+                }
+
+
+                $code = 'ESLINT.'.$prefix.'.'.$rule;
 
                 $message = new ArcanistLintMessage();
                 $message->setPath($path);
                 $message->setLine(intval($child->getAttribute('line')));
                 $message->setChar(intval($child->getAttribute('column')));
                 $message->setCode($code);
-                $message->setDescription($child->getAttribute('message'));
+                $message->setDescription($description);
                 $message->setSeverity($this->getLintMessageSeverity($code));
+                $message->setName($name);
 
                 $messages[] = $message;
             }
